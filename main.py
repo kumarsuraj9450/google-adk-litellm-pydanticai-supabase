a89b2da10560d93999932e6d902a338721a12539
Kumar Suraj <kumarsuraj9450@gmail.com>
2025-11-11 22:30:20 +0530

    added .env

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-def main():
-    print("Hello from hello-world!")
-
 from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIChatModel
 from pydantic_ai.providers.litellm import LiteLLMProvider
+from dotenv import load_dotenv
+import os
 
-model = OpenAIChatModel(
-    'gemini/gemini-2.5-flash-preview-09-2025',
-    provider=LiteLLMProvider(
-        api_base='http://localhost:4000/',
-        api_key='sk-UD9zG2pjPEzIs6M865L_Gw'
+load_dotenv()
+
+
+def main():
+    print("Hello from hello-world!")
+
+    model = OpenAIChatModel(
+        'gemini/gemini-2.5-flash-preview-09-2025',
+        provider=LiteLLMProvider(
+            api_base=LITELLM_GEMINI_URL,
+            api_key=LITELLM_GEMINI_VIRTUAL_KEY
+        )
     )
-)
-agent = Agent(model)
+    agent = Agent(model)
 
-result = agent.run_sync('What is the capital of France?')
-print(result.output)
-#> The capital of France is Paris.
-...
+    result = agent.run_sync('What is the capital of France?')
+    return result
+
 if __name__ == "__main__":
-    main()
+    LITELLM_GEMINI_VIRTUAL_KEY = os.environ.get("LITELLM_GEMINI_VIRTUAL_KEY","")
+    LITELLM_GEMINI_URL = os.environ.get("LITELLM_GEMINI_URL","")
+    print(LITELLM_GEMINI_VIRTUAL_KEY,LITELLM_GEMINI_URL)
+    result = main()
+    print(result.output)
+
+
--- supabase-test.py
+++ supabase-test.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("SUPABASE_URL","")
+key: str = os.environ.get("SUPABASE_KEY","")
 supabase: Client = create_client(url, key)
 
 response = (
